b41fda7d55a30ecf047acfd804e34a8f7990658c
Ubuntu <ubuntu@ip-172-31-33-37.eu-central-1.compute.internal>
2019-09-26 08:09:26 +0000

    script with loop for

--- clrlogs.sh
+++ clrlogs.sh
@@ -5,19 +5,21 @@
 #
 ##############################################################################################################
 
-LOG_DIR=/some/log/dir
-DAYS_TO_HOLD=7
+LOG_DIR=/var/log
+MINS_TO_HOLD=1
 NOW=`date +%Y%m%d`
-
+FILE_MASK=$1
+REM_USER=ubuntu
+REM_SERVER=18.196.139.93
+DEST_DIR=/tmp
 echo "Starting log cleanup process ..."
-
-find ${LOG_DIR} -name "*your_file_pattern_match_here*" -mtime +${DAYS_TO_HOLD} -exec aws s3 mv {} s3://somebucket-that-holds-logs \; >/dev/null 2>&1
-
-echo "Log clean up completed"
-
-
+for i in $(find $LOG_DIR -name $FILE_MASK.log* -type f -mmin +1)
+do 
+	scp $i $REM_USER@$REM_SERVER:/$DEST_DIR/
+done
 
 
 
-
+#tar cvf ex.tar.gz "/home/sid/ex"
+#find -name "ex.tar.gz" -type -f -mmin +3 -exec rm ex.tar.gz\; 
 
