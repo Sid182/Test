f1fbc3b791b9073016fbb1b18c2e0c6a46ba7cba
Ubuntu <ubuntu@ip-172-31-33-37.eu-central-1.compute.internal>
2019-09-26 09:48:49 +0000

    loops with condition

--- clrlogs.sh
+++ clrlogs.sh
@@ -1,25 +1,30 @@
-
-##############################################################################################################
-#
-# this script will move any log files that are older than 7 days to s3
-#
-##############################################################################################################
-
 LOG_DIR=/var/log
 MINS_TO_HOLD=1
 NOW=`date +%Y%m%d`
 FILE_MASK=$1
 REM_USER=ubuntu
 REM_SERVER=18.196.139.93
-DEST_DIR=/tmp
-echo "Starting log cleanup process ..."
-for i in $(find $LOG_DIR -name $FILE_MASK.log* -type f -mmin +1)
+DEST_DIR=/tmp/backup
+echo "
+##############################################################################################################
+#
+# this script will move any log files that are older than x days to remote server
+#
+##############################################################################################################
+"
+for i in $(find $LOG_DIR -name $FILE_MASK.log* -type f -mtime +15)
 do 
-	scp $i $REM_USER@$REM_SERVER:/$DEST_DIR/
+if ssh $REM_USER@$REM_SERVER "[ ! -d $DEST_DIR ]"; then
+echo "Creating Destination directory"
+ssh $REM_USER@$REM_SERVER "mkdir $DEST_DIR"
+echo "copying logs" 
+scp $i $REM_USER@$REM_SERVER:/$DEST_DIR/
+else
+scp $i $REM_USER@$REM_SERVER:/$DEST_DIR/
+fi
 done
 
 
 
-#tar cvf ex.tar.gz "/home/sid/ex"
-#find -name "ex.tar.gz" -type -f -mmin +3 -exec rm ex.tar.gz\; 
+
 
