0ae2b163bf68e518e1b3f32c552efa098a05304e
Ubuntu <ubuntu@ip-172-31-33-37.eu-central-1.compute.internal>
2019-09-26 14:44:25 +0000

    moving logs to remote server

--- clrlogs.sh
+++ clrlogs.sh
@@ -6,20 +6,22 @@ echo "
 ##############################################################################################################
 "
 LOG_DIR=/var/log
-DAYS_TO_HOLD=+15
+DAYS_TO_HOLD=+3
 FILE_MASK=$1
 REM_USER=ubuntu
 REM_SERVER=18.196.139.93
 DEST_DIR=/tmp/backup_logs
 echo "Copying logs from $LOG_DIR to $REM_SERVER:$DEST_DIR"
-for i in $(find $LOG_DIR -name $FILE_MASK.log* -type f -mtime $DAYS_TO_HOLD)
+for i in $(find $LOG_DIR -name $FILE_MASK.log* -type f -mmin $DAYS_TO_HOLD)
 do 
 if ssh $REM_USER@$REM_SERVER "[ ! -d $DEST_DIR ]"; then
 echo "Creating Destination directory"
 ssh $REM_USER@$REM_SERVER "mkdir $DEST_DIR"
-scp $i $REM_USER@$REM_SERVER:/$DEST_DIR/ 
+rsync -avz --remove-source-files -e ssh $i $REM_USER@$REM_SERVER:/$DEST_DIR/
+#scp $i $REM_USER@$REM_SERVER:/$DEST_DIR/ 
 else 
-scp $i $REM_USER@$REM_SERVER:/$DEST_DIR/ 
+rsync -avz --remove-source-files -e ssh $i $REM_USER@$REM_SERVER:/$DEST_DIR/
+#scp $i $REM_USER@$REM_SERVER:/$DEST_DIR/ 
 fi
 done
 echo "Logs is successfully copied to destination folder"
@@ -33,6 +35,3 @@ echo "
 #
 ##############################################################################################################
 "
-
-
-
