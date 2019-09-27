d1b079b99217d2336d1e011e765088dc7ea5e7ac
Ubuntu <ubuntu@ip-172-31-33-37.eu-central-1.compute.internal>
2019-09-27 10:48:26 +0000

    file_type as parametr

--- clrlogs.sh
+++ clrlogs.sh
@@ -5,29 +5,30 @@ echo "
 #
 ##############################################################################################################
 "
+
+FILE_MASK=$1
+FILE_TYPE=$2
 LOG_DIR=/var/log
 DAYS_TO_HOLD=+1
-FILE_MASK=$1
 REM_USER=ubuntu
 REM_SERVER=18.196.139.93
 DEST_DIR=/tmp/backup_logs
-whoami
-echo "Copying logs from $LOG_DIR to $REM_SERVER:$DEST_DIR"
-for i in $(find $LOG_DIR -name $FILE_MASK.log* -type f -mmin $DAYS_TO_HOLD)
-do 
+
+
 if ssh $REM_USER@$REM_SERVER "[ ! -d $DEST_DIR ]"; then
 echo "Creating Destination directory"
 ssh $REM_USER@$REM_SERVER "mkdir $DEST_DIR"
-echo "Copy $i from $LOG_DIR to $REM_SERVER:$DEST_DIR"
-#rsync -havz --remove-source-files -e ssh $i $REM_USER@$REM_SERVER:/$DEST_DIR/
-#scp $i $REM_USER@$REM_SERVER:/$DEST_DIR/ 
-else 
-echo "Copy $i from $LOG_DIR to $REM_SERVER:$DEST_DIR"
-#rsync -havz --remove-source-files -e ssh $i $REM_USER@$REM_SERVER:/$DEST_DIR/
-scp $i $REM_USER@$REM_SERVER:/$DEST_DIR/ 
 fi
+
+echo "Copying logs from $LOG_DIR to $REM_SERVER:$DEST_DIR"
+for i in $(find $LOG_DIR -name $FILE_MASK.$FILE_TYPE -type f -mmin $DAYS_TO_HOLD)
+do 
+echo "Copy $i from $LOG_DIR to $REM_SERVER:$DEST_DIR"
+rsync -havz --remove-source-files -e ssh $i $REM_USER@$REM_SERVER:/$DEST_DIR/
+#scp $i $REM_USER@$REM_SERVER:/$DEST_DIR/ 
+
 done
-echo "Logs is successfully copied to destination folder"
+
 echo "List of copied logs"
 ssh $REM_USER@$REM_SERVER  "cd $DEST_DIR && ls -l"
 echo "
