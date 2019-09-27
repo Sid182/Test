1cfd2a45fd274bd58fc4578a065f042ba408c2b3
Ubuntu <ubuntu@ip-172-31-33-37.eu-central-1.compute.internal>
2019-09-27 12:24:42 +0000

    adding quotes to parameters in find operator

--- clrlogs.sh
+++ clrlogs.sh
@@ -1,4 +1,4 @@
-echo "
+\echo "
 ##############################################################################################################
 #
 # this script will move any log files that are older than x days to remote server
@@ -9,19 +9,18 @@ echo "
 FILE_MASK=$1
 FILE_TYPE=$2
 LOG_DIR=/var/log
-DAYS_TO_HOLD=+1
+MINS_TO_HOLD=+1
 REM_USER=ubuntu
 REM_SERVER=18.196.139.93
 DEST_DIR=/tmp/backup_logs
-
 
 if ssh $REM_USER@$REM_SERVER "[ ! -d $DEST_DIR ]"; then
 echo "Creating Destination directory"
 ssh $REM_USER@$REM_SERVER "mkdir $DEST_DIR"
 fi
 
-echo "Copying logs from $LOG_DIR to $REM_SERVER:$DEST_DIR"
-for i in $(find $LOG_DIR -name $FILE_MASK.$FILE_TYPE -type f -mmin $DAYS_TO_HOLD)
+echo "======================Copying logs======================"
+for i in $(find $LOG_DIR -name "$FILE_MASK.$FILE_TYPE" -type f -mmin $MINS_TO_HOLD)
 do
 echo "Copy $i from $LOG_DIR to $REM_SERVER:$DEST_DIR"
 rsync -havz --remove-source-files -e ssh $i $REM_USER@$REM_SERVER:/$DEST_DIR/
