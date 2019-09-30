83d58fdcff054fdc26700c474d96c6c02373418c
Sid182 <47497242+Sid182@users.noreply.github.com>
2019-09-30 10:22:45 +0300

    Update clrlogs.sh

--- clrlogs.sh
+++ clrlogs.sh
@@ -9,7 +9,7 @@
 FILE_MASK=$1
 FILE_TYPE=$2
 LOG_DIR=$3
-MINS_TO_HOLD=$4
+DAYS_TO_HOLD=$4
 REM_USER=$5
 REM_SERVER=$6
 DEST_DIR=$7
@@ -19,14 +19,14 @@ echo "Creating Destination directory"
 ssh $REM_USER@$REM_SERVER "mkdir $DEST_DIR"
 fi
 
-echo "======================Copying logs======================"
-for i in $(find $LOG_DIR -name "$FILE_MASK.$FILE_TYPE" -type f -mmin $MINS_TO_HOLD)
+echo "======================Moving logs======================"
+for i in $(find $LOG_DIR -name "$FILE_MASK.$FILE_TYPE" -type f -mtime $DAYS_TO_HOLD)
 do
 echo "Copy $i from $LOG_DIR to $REM_SERVER:$DEST_DIR"
-rsync -havz --remove-source-files -e ssh $i $REM_USER@$REM_SERVER:/$DEST_DIR/
+rsync -az --remove-source-files -e ssh $i $REM_USER@$REM_SERVER:/$DEST_DIR/
 done
 
-echo "List of copied logs"
+echo "List of moving files"
 ssh $REM_USER@$REM_SERVER  "cd $DEST_DIR && ls -l"
 echo "
 ##############################################################################################################
