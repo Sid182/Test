44d5cb3d639873bd55dea638f9f86d5ba08c0c40
Sid182 <47497242+Sid182@users.noreply.github.com>
2019-11-07 12:35:36 +0200

    Create start_stop_status_aws.sh

---
+++ start_stop_status_aws.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#function usage() {
+#  echo "$0 (start|stop|status)"
+#}
+
+#if [ $# -ne 1 ]; then
+#  usage
+#  exit 2
+#fi
+ACTION=$1
+EC2_LIST_ID=$2
+DEBUG="echo "
+DEBUG=""
+REGION=eu-central-1
+
+INSTANCE_STS=`aws ec2 describe-instance-status --region $REGION --instance-ids ${EC2_LIST_ID[@]} | jq -r '.InstanceStatuses[].InstanceState.Name'`
+
+echo "InstanceStatus : $INSTANCE_STS"
+
+EXIT=0
+for i in  $INSTANCE_STS
+do
+if [ "$1" = "start" ]; then
+  if [ "$INSTANCE_STS" = "running" ]; then
+    echo "instance is already running."
+  else
+    echo "start instance..."
+    $DEBUG aws ec2 start-instances --instance-ids ${EC2_LIST_ID[@]} --region $REGION
+    EXIT=$?
+  fi
+elif [ "$1" = "stop" ]; then
+  if [ "$i" = "running" ]; then
+    echo "stop instance..."
+    $DEBUG aws ec2 stop-instances --instance-ids ${EC2_LIST_ID[@]} --region $REGION
+    EXIT=$?
+  else
+    echo "instance is already stopped."
+  fi
+elif [ "$1" = "status" ]; then
+  echo "Status Check only."
+#else
+#  usage
+#  EXIT=2
+fi
+
+exit $EXIT
+done
